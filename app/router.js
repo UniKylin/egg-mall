820a3b09673a8ff0c5f105e6160660bb56a5b5a9
Kylin <kylin@KylindeMacBook-Pro.local>
2020-08-26 23:46:44 +0800

    login page style

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
 
+  router.get('/login', controller.admin.login.index)
 
   router.get('/manager', controller.admin.manager.index)
   router.get('/manager/add', controller.admin.manager.add)
