433101f0ba1d393c06cb2f84f81677e69323cd8b
UniKylin <unikylin@126.com>
2020-09-02 21:36:34 +0800

    feat: add admin manager

--- app/controller/admin/manager.js
+++ app/controller/admin/manager.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const Controller = require('egg').Controller;
+const BaseController = require('./base')
 
-class ManagerController extends Controller {
+class ManagerController extends BaseController {
   async index() {
     await this.ctx.render(
       `admin/manager/index`,
@@ -13,7 +13,25 @@ class ManagerController extends Controller {
   }
 
   async add() {
-    await this.ctx.render(`admin/manager/add`)
+    const roleList = await this.ctx.model.Role.find()
+    console.log(roleList)
+    await this.ctx.render(`admin/manager/add`, { roleList })
+  }
+
+  async doAdd() {
+    const admin = this.ctx.request.body
+    const existAdmin = await this.ctx.model.Admin.find({ username: admin.username })
+    admin.password = await this.service.tools.encodeMd5(admin.password)
+
+    console.log(`------`)
+    console.log(existAdmin)
+    if (existAdmin && existAdmin.length > 0) {
+      await this.error('/admin/manager', `管理员: ${admin.username} 已经存在了`)
+    } else {
+      console.log(admin)
+      await this.ctx.model.Admin(admin).save()
+      await this.success('/admin/manager', `管理员添加成功`)
+    }
   }
 
   async edit() {
